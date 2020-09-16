53beba98727ea91f12e3825803fdad3b48b2dd41
ferfreitas <71359128+ferfreitas@users.noreply.github.com>
2020-09-15 22:05:46 -0300

    Create 1012.py

---
+++ 1012.py
@@ -0,0 +1,17 @@
+A,B,C = input().split(" ")
+
+A = float(A)
+B = float(B)
+C = float(C)
+
+triangulo = (A * C) / 2
+circulo = 3.14159 * C ** 2
+trapezio = ((A + B) * C) / 2
+quadrado = B * B
+retangulo = A * B
+
+print("TRIANGULO: {:.3f}".format(triangulo))
+print("CIRCULO: {:.3f}".format(circulo))
+print("TRAPEZIO: {:.3f}".format(trapezio))
+print("QUADRADO: {:.3f}".format(quadrado))
+print("RETANGULO: {:.3f}".format(retangulo))
