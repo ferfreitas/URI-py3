18b8dcd4f6011776e4bd32695aed3936e2db8b23
ferfreitas <71359128+ferfreitas@users.noreply.github.com>
2020-09-15 22:03:55 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,8 @@
+numero = int(input())
+horas_trab = int(input())
+valor_hora = float(input())
+
+salario = horas_trab * valor_hora
+
+print("NUMBER =", numero)
+print("SALARY = U$ {:.2f}".format(salario))
